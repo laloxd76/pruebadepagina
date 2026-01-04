124f3049a76919786cdd427d850d23ee69c2c328
Fidel <martinezcrack62@gmail.com>
2026-01-04 13:45:41 -0600

    act

---
+++ MiPerfumeria/datos.js
@@ -0,0 +1,40 @@
+const listadeperfumes = [
+    { 
+        nombre: "Dior Sauvage",
+        genero: "hombre",
+        descripcion: "Fragancia Increiblemente Sexy",
+        imagen: "https://www.dior.com/dw/image/v2/BGXS_PRD/on/demandware.static/-/Library-Sites-DiorSharedLibrary/default/dwfb761903/images/beauty/01-FRAGRANCES/2025/PDP-REVAMP/SAUVAGE/Y0996460/POSTER_CAPSULE_Elixir_VA_916.jpg?sw=800",
+        precio: "$2,800 mxn",
+        color: "#021123",
+        fuente: "serif",
+        premium: true
+    },
+    {
+        nombre: "Bleu de Chanel",
+        genero: "hombre",
+        descripcion: "Elegancia pura",
+        imagen: "https://www.chanel.com/puls-img/q_auto:good,f_auto,dpr_1.1/1760520673129-backup-3d-bleu-1x1_1080x1080.png",
+        precio: "$3,100 mxn",
+        fuente: "serif",
+        premium: true
+    },
+    {
+        nombre: "Miss Dior",
+        genero: "mujer",
+        descripcion: "Dulce y floral",
+        imagen: "https://www.dior.com/dw/image/v2/BGXS_PRD/on/demandware.static/-/Sites-dior-master-catalog/default/dw5f2d7a22/Y0996429/Y0996429_C099600984_E01_ZHC.jpg?sw=800",
+        precio: "$2,950 mxn",
+        color: "#f8bbd0",
+        premium: true
+    },
+    {
+        nombre: "Loción de Spiderman",
+        genero: "hombre",
+        descripcion: "Locion de niño",
+        imagen: "https://republiccosmetics.com/cdn/shop/files/Perfume_Spiderman_510x.progressive.jpg?v=1741796869",
+        precio: "$150 mxn",
+        fuente: "serif",
+        premium: false
+    }
+    // Agrega aquí más perfumes si quieres...
+];
