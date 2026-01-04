6a94663cc5438057d74234c1fbff2bf22f07917d
Fidel <martinezcrack62@gmail.com>
2026-01-04 15:20:29 -0600

    act

--- datos.js
+++ datos.js
@@ -37,12 +37,13 @@ const listadeperfumes = [
         premium: false
     },
     {
-                nombre: "Versace Eros EDP",
+        nombre: "Versace Eros EDP",
         genero: "hombre",
         descripcion: "Fragancia de Vainilla",
         imagen: "https://www.versace.com/on/demandware.static/-/Library-Sites-ver-library/default/dwbf3cbf93/EROS.jpg",
         precio: "$3,200 mxn",
         fuente: "serif",
+        color:"#76E3E3",
         premium: true
     }
 ];
